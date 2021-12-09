08c3a94cf81c3a421f0981707a7de8576cb2ca92
Amit Vashist <amitvashist7@outlook.com>
2021-12-09 04:21:06 +0000

    15-Functions

---
+++ 15-Functions/resources.tf
@@ -0,0 +1,24 @@
+locals { 
+  default = timestamp()
+}
+
+
+locals { 
+  time = formatdate("DD MMM YYYY hh:mm ZZZ", "2018-01-02T23:12:01Z")
+}
+
+locals { 
+  timenow = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+}
+
+output "fmttime"{ 
+  value  = local.time
+}
+
+output "timenow"{ 
+  value  = local.timenow
+}
+
+output "default"{ 
+  value  = local.default
+}
